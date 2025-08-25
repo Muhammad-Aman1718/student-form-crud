e3e5551d1a034f482d8b4ed760bf78d4bf17c8ce
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-08-26 02:13:59 +0500

    working on back end

---
+++ Student-Back-end/src/index.ts
@@ -0,0 +1,32 @@
+// src/index.ts
+import express from "express";
+import cors from "cors";
+import userRoutes from "./routes/userRoutes";
+
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Server is running successfully!",
+    status: "OK",
+  });
+});
+
+// User routes ko use karna - '/api/users' base path ke saath
+app.use("/api/users", userRoutes);
+
+// 404 handler - agar koi route nahi mila
+// app.use("*", (req, res) => {
+//   res.status(404).json({
+//     error: "Route not found",
+//     message: `Cannot ${req.method} ${req.originalUrl}`,
+//   });
+// });
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
---
+++ Student-Back-end/src/routes/userRoutes.ts
@@ -0,0 +1,19 @@
+// src/routes/userRoutes.ts
+import { Router } from "express";
+import { prisma } from "../config/prisma";
+
+const router = Router();
+
+// ✅ GET all users
+router.get("/", async (req, res) => {
+  try {
+    const users = await prisma.student.findMany();
+    console.log("this is users  --->", users);
+
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch users" });
+  }
+});
+
+export default router;
