7b30ee2206b6d53c7ac7426b16feaf4b105d1492
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-08-27 19:33:23 +0500

    API's is completed

--- Student-Back-end/src/routes/student.ts
+++ Student-Back-end/src/routes/student.ts
@@ -1,6 +1,7 @@
 // src/routes/userRoutes.ts
 import { Router } from "express";
 import { prisma } from "../config/prisma";
+import { log } from "console";
 
 const router = Router();
 
@@ -10,7 +11,7 @@ router.get("/", async (req, res) => {
     const students = await prisma.student.findMany();
     console.log("this is students  --->", students);
 
-    res.json(students);
+    res.status(200).json(students);
   } catch (error) {
     res.status(500).json({ error: "Failed to fetch students" });
   }
@@ -28,6 +29,38 @@ router.post("/", async (req, res) => {
     res.status(201).json(newStudent);
   } catch (error) {
     res.status(500).json({ error: "Failed to create student" });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    console.log("this is params==============>", req);
+
+    const { id } = req.params;
+    console.log("this is id  --->", id);
+
+    const updatedStudent = await prisma.student.update({
+      where: { id },
+
+      data: req.body,
+    });
+    console.log("this is updatedStudent  --->", updatedStudent);
+
+    res.json(updatedStudent);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to update student" });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    await prisma.student.delete({
+      where: { id },
+    });
+    res.status(204).end();
+  } catch (error) {
+    res.status(500).json({ error: "Failed to delete student" });
   }
 });
 
